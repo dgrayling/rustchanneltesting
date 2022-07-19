be32e492015f5f962456f08c16d1b7496b2437aa
vimes <daniel.grayling.2020@gmail.com>
2022-07-19 17:19:21 +0100

    Refactor

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,7 @@ fn getThing() -> (Sender<i32>,std::boxed::Box<Thing>) {
     return (tx,boxedThing);
 }
 
-fn startThingThread(thing: &mut Thing) -> std::thread::JoinHandle<()> {
+fn startThingThread(mut thing: Box<Thing>) -> std::thread::JoinHandle<()> {
     let handle = thread::spawn(move || {
         for received in thing.rx {
             thing.received = received;
@@ -36,26 +36,15 @@ fn main() {
     let (tx1, mut thing1) = getThing();
     let (tx2, mut thing2) = getThing();
 
-    let h1 = thread::spawn(move || {
-        for received in thing1.rx {
-            thing1.received = received;
-            println!("Got: {}", received);
-            println!("Got thing received: {}", thing1.received);
-        }
-    });
-
-    let h2 = thread::spawn(move || {
-        for received in thing2.rx {
-            println!("Got: {}", received);
-        }
-    });
+    let h1 = startThingThread(thing1);
+    let h2 = startThingThread(thing2);
 
     let h3 = thread::spawn(move || {
         tx1.send(2).unwrap();
     });
 
     let h4 = thread::spawn(move || {
-        tx2.send(2).unwrap();
+        tx2.send(3).unwrap();
     });
 
     h1.join().unwrap();
